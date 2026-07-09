c23baf6a0a18bc550e1533f334624683363d33a7
KrumKK <krum@lizarte.com>
2026-07-09 08:32:00 +0200

    Build 89 - Estilo seleccionado neutro para No prioritario

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('./presentations-slides-manifest.js');
 
-const CACHE_VERSION = 'v88';
+const CACHE_VERSION = 'v89';
 const CACHE_NAME = 'mision-china-' + CACHE_VERSION;
 
 function manifestSlideAssets() {
